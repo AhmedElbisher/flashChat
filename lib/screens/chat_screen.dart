c79803edf0c77a6d534e6706677e68c9d304a7dc
Ahmed El-Bisher <ah.bisher014@gmail.com>
2020-06-11 12:36:09 +0200

    retreive massages and polishing ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
+FirebaseUser currentUser;
+Firestore _firestore = Firestore.instance;
+FirebaseAuth _auth = FirebaseAuth.instance;
+
 class ChatScreen extends StatefulWidget {
   static String id = "ChatScreen";
   @override
@@ -11,10 +15,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  Firestore _firestore = Firestore.instance;
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  FirebaseUser currentUser;
   String messageText;
+  TextEditingController textEditingController = TextEditingController();
   void getuserInfo() async {
     try {
       currentUser = await _auth.currentUser();
@@ -54,28 +56,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection("messsages").snapshots(),
-              builder: (context, snapshat) {
-                if (!snapshat.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                List<MassageBubble> messageBubbles = [];
-                for (var document in snapshat.data.documents) {
-                  String messageText = document.data["text"];
-                  String messageSender = document.data["sender"];
-                  messageBubbles.add(MassageBubble(
-                    text: messageText,
-                    email: messageSender,
-                  ));
-                }
-                return Expanded(
-                  child: ListView(
-                    children: messageBubbles,
-                  ),
-                );
-              },
-            ),
+            MassagesListBuilder(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -83,6 +64,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: textEditingController,
                       onChanged: (value) {
                         messageText = value;
                         //Do something with the user input.
@@ -92,6 +74,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () async {
+                      textEditingController.clear();
                       await _firestore.collection("messsages").add(
                           {"text": messageText, "sender": currentUser.email});
                       //Implement send functionality.
@@ -111,37 +94,82 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+class MassagesListBuilder extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MassagesListBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection("messsages").snapshots(),
+      builder: (context, snapshat) {
+        if (!snapshat.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+        List<MassageBubble> messageBubbles = [];
+        for (var document in snapshat.data.documents.reversed) {
+          String messageText = document.data["text"];
+          String messageSender = document.data["sender"];
+          messageBubbles.add(MassageBubble(
+            text: messageText,
+            email: messageSender,
+            isMe: messageSender == currentUser.email,
+          ));
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
 class MassageBubble extends StatelessWidget {
   final String text;
   final String email;
-  MassageBubble({this.text, this.email});
+  final bool isMe;
+  MassageBubble({this.text, this.email, this.isMe});
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
+      crossAxisAlignment:
+          isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
       children: [
         Padding(
-          padding: const EdgeInsets.only(right: 8.0),
+          padding: const EdgeInsets.only(right: 10.0, left: 10.0),
           child: Text(
             email,
             style: TextStyle(
               color: Colors.black54,
-              fontSize: 15.0,
+              fontSize: 10.0,
             ),
           ),
         ),
         Padding(
           padding: EdgeInsets.only(top: 2.0, right: 10.0, bottom: 10.0),
           child: Material(
-              borderRadius: BorderRadius.circular(20.0),
+              borderRadius: isMe
+                  ? BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                      bottomRight: Radius.circular(15))
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                      bottomRight: Radius.circular(15)),
               elevation: 5.0,
-              color: Colors.lightBlue,
+              color: isMe ? Colors.white : Colors.lightBlue,
               child: Padding(
                 padding: EdgeInsets.all(8.0),
                 child: Text(
                   text,
                   style: TextStyle(
-                    color: Colors.white,
+                    color: isMe ? Colors.black54 : Colors.white,
                     fontSize: 16.0,
                   ),
                 ),
